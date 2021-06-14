cb31cf7cf7ba1471f7d4e9bc0c6639ca7fe094dd
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-06-14 21:55:47 +0530

    Started with functionality

---
+++ index.ts
@@ -0,0 +1,5 @@
+// const click = document.getElementById('click') ; 
+let click= document.getElementById('click')!; 
+let contentDivs = document.querySelectorAll('.content')  
+
+click.onclick = () => alert('HI') ; 
