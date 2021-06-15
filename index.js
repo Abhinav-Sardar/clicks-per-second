7bada909fa9cee1fe0d56a95770d490e1c7ce6ea
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-06-15 20:41:09 +0530

    Basic clicking functionality done

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 "use strict";
-// const click = document.getElementById('click') ; 
-let click = document.getElementById('click');
-let contentDivs = document.querySelectorAll('.content');
-click.onclick = () => alert('HI');
+const click = document.getElementById('click');
+let time = 0;
+let cps = 0;
+let clicks = 0;
+const content = document.querySelectorAll('.content');
+let isStarted = false;
+let statusString = "Click here to start testing";
+setInterval(() => {
+    content.forEach((element, index) => {
+        element.innerHTML = String([time, cps, clicks][index]);
+    });
+    click.innerText = statusString;
+    if (time === 0 || clicks === 0) {
+        cps = 0;
+    }
+    else {
+        cps = Number((clicks / time).toFixed(2));
+    }
+});
+const handleClick = () => {
+    if (isStarted === false) {
+        statusString = 'Click!!!';
+        isStarted = true;
+        clicks++;
+        setTimeout(() => {
+            time += 1;
+            let timeSetter = setInterval(() => {
+                time++;
+                if (time === 5) {
+                    clearInterval(timeSetter);
+                    click.disabled = true;
+                }
+            }, 1000);
+        }, 1000);
+    }
+    else {
+        clicks++;
+    }
+};
+click.addEventListener('click', handleClick);
--- index.ts
+++ index.ts
@@ -1,5 +1,50 @@
-// const click = document.getElementById('click') ; 
-let click= document.getElementById('click')!; 
-let contentDivs = document.querySelectorAll('.content')  
+const click = document.getElementById('click')! as HTMLButtonElement ; 
+let time:number = 0 ; 
+let cps:number =0 ; 
+let clicks:number = 0 ;
+const content = document.querySelectorAll('.content') ; 
+let isStarted:boolean = false ; 
+let statusString:string = "Click here to start testing"
 
-click.onclick = () => alert('HI') ; 
+
+setInterval(() => {
+    content.forEach((element , index) => {
+        element.innerHTML = String([time , cps , clicks][index]) ; 
+    });
+    click.innerText = statusString ; 
+
+    if(time === 0 || clicks === 0){
+        cps = 0 ; 
+    }
+    else {
+        cps = Number((clicks/time).toFixed(2)) ; 
+    }
+
+})
+
+const handleClick:() => void = () =>{ 
+    if(isStarted === false){
+        statusString = 'Click!!!'
+        isStarted = true ;
+        clicks++ ; 
+        setTimeout(() => {
+            time += 1 ; 
+            let timeSetter:NodeJS.Timeout = setInterval(() => {
+                    time++ ;
+                    if(time === 5){
+                        clearInterval(timeSetter) ; 
+                        click.disabled = true ; 
+                    }
+            } , 1000)
+        } , 1000) ; 
+
+    }
+    else {
+        clicks++ ; 
+    }
+}
+
+
+
+click.addEventListener('click' , handleClick) ;
+
